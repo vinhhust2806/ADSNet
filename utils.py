bb2a32bccc231a61332a6fbfcdfcb8f1802cfc2b
Quang-Vinh Nguyen <88282800+vinhhust2806@users.noreply.github.com>
2024-07-07 18:19:16 +0900

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,35 @@
+import torch.nn as nn
+import torch
+
+def dice_coef_loss(pred, label):
+    smooth = 1.0
+    intersection = 2.0 * (pred * label).sum() + smooth
+    union = pred.sum() + label.sum() + smooth
+    return 1 - (intersection / union)
+
+def bce_dice_loss(pred, label):
+    dice_loss = dice_coef_loss(pred, label)
+    bce_loss = nn.BCELoss()(pred, label)
+    return dice_loss + bce_loss
+
+def dice_coefficient(y_true, y_pred, eps=1e-9):
+    intersection = torch.sum(y_true * y_pred)
+    union = torch.sum(y_true) + torch.sum(y_pred)
+    dice_score = (2. * intersection + eps) / (union + eps)
+    return dice_score
+
+def iou_score(y_true, y_pred, eps=1e-9):
+    intersection = torch.sum(y_true * y_pred)
+    union = torch.sum(y_true) + torch.sum(y_pred) - intersection
+    iou = (intersection + eps) / (union + eps)
+    return iou
+
+def mae_score(y_true, y_pred):
+    mae = torch.mean(torch.abs(y_true - y_pred))
+    return mae
+
+def adjust_lr(optimizer, initial_lr, epoch, decay_rate=0.1, decay_epoch=40):
+    decay = decay_rate ** (epoch // decay_epoch)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = initial_lr * decay
+        print("Learning rate: ", param_group['lr'])
