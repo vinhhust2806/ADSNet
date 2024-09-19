7d62a885f4d38a897230e41cf74bd905d79d03d4
Quang-Vinh Nguyen <88282800+vinhhust2806@users.noreply.github.com>
2024-09-19 14:22:09 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,17 +8,6 @@ def dice_coef_metric(pred, label):
         return 1.
     return intersection / union
     
-def dice_coef_loss(pred, label):
-    smooth = 1.0
-    intersection = 2.0 * (pred * label).sum() + smooth
-    union = pred.sum() + label.sum() + smooth
-    return 1 - (intersection / union)
-    
-def bce_dice_loss(pred, label):
-    dice_loss = dice_coef_loss(pred, label)
-    bce_loss = nn.BCELoss()(pred, label)
-    return dice_loss + bce_loss
-
 def dice_coefficient(y_true, y_pred, eps=1e-9):
     intersection = torch.sum(y_true * y_pred)
     union = torch.sum(y_true) + torch.sum(y_pred)
@@ -34,6 +23,17 @@ def iou_score(y_true, y_pred, eps=1e-9):
 def mae_score(y_true, y_pred):
     mae = torch.mean(torch.abs(y_true - y_pred))
     return mae
+
+def dice_coef_loss(pred, label):
+    smooth = 1.0
+    intersection = 2.0 * (pred * label).sum() + smooth
+    union = pred.sum() + label.sum() + smooth
+    return 1 - (intersection / union)
+    
+def bce_dice_loss(pred, label):
+    dice_loss = dice_coef_loss(pred, label)
+    bce_loss = nn.BCELoss()(pred, label)
+    return dice_loss + bce_loss
 
 def adjust_lr(optimizer, initial_lr, epoch, decay_rate=0.1, decay_epoch=40):
     decay = decay_rate ** (epoch // decay_epoch)
